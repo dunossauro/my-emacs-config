cd5ee087c12b57428fa711213f00af9e3fdb4fb1
z4r4tu5tr4 <mendesxeduardo@gmail.com>
2018-02-18 21:45:30 -0300

    confortable config

--- init.el
+++ init.el
@@ -1,42 +1,18 @@
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
+;; Emacs configs.
 
-;; Remove welcome message
 (package-initialize)
 
+;; Remove welcome message
 (setq inhibit-startup-message t)
-
-;; ----------------------------------------------------------------------
-;; Emacs Theme
-;;
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(ansi-color-faces-vector
-   [default default default italic underline success warning error])
- '(ansi-color-names-vector
-   ["#2e3436" "#a40000" "#4e9a06" "#c4a000" "#204a87" "#5c3566" "#729fcf" "#eeeeec"])
- '(custom-enabled-themes (quote (tsdh-dark)))
- '(package-selected-packages
-   (quote
-    (flycheck python-docstring flymake-python-pyflakes python multishell neotree ##))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-
 ;; ----------------------------
 ;; term macro in new window
 ;; Shell in a new vertical windows
 ;; TODO
 
-;; -----------------------
+;; Remove menus
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+
 ;; MELPA
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
@@ -53,6 +29,28 @@
 (setq flymake-python-pyflakes-executable "flake8")
 
 ;; Flyc
-
 (add-hook 'after-init-hook #'global-flycheck-mode)
 
+;; Theme
+(load-theme 'darkside t)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("7334f761edfccfa87893211f651220a0899bb206f042b856393b7af22b0413e2" default)))
+ '(package-selected-packages
+   (quote
+    (ergoemacs-mode ergoemacs-status python-docstring python neotree multishell flymake-python-pyflakes flycheck))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+;; ergoemacs
+(require 'ergoemacs-status)
+(ergoemacs-status-mode)
